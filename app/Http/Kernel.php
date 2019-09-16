3a0d03d0126846d32579313a4f2a53dfebe79e94
rafi <rafiseptian177@gmail.com>
2019-09-16 20:37:41 +0700

    V1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'custom_auth' => \App\Http\Middleware\CustomAuthMiddleware::class,
+        'isNotAdmin' => \App\Http\Middleware\UserMiddleware::class,
     ];
 
     /**
